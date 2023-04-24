c064dd5ad084aefae241a12b17cc4d1ae73d6abb
jeannie <jnam820@gmail.com>
2023-04-24 16:19:12 +0800

    test

--- threex.text.js
+++ threex.text.js
@@ -1,31 +1,37 @@
 var THREEx	= THREEx	|| {}
 
+//var fontLoader = new THREE.FontLoader();
+//onst optimer = fontLoader.load('fonts/optimer_bold.typeface.json')
 
 THREEx.Text	= function(text, options){
 	options	= options || {
-		font		: "droid serif",
+		font		: "optimer",
 		weight		: "bold",
 		size		: 1,
-		height		: 0.4,
+		height		: 1,
+		curveSegments: 10,
+		bevelThickness: 1,
+		bevelSize: 0.05
 	}
 
-	// create the tGeometry
 	var geometry	= new THREE.TextGeometry(text, options)
 
-	// center the geometry
-	// - THREE.TextGeometry isnt centered for unknown reasons. all other geometries are centered
 	geometry.computeBoundingBox();
 	var center	= new THREE.Vector3();
 	center.x	= (geometry.boundingBox.max.x - geometry.boundingBox.min.x) / 2
-	// center.y	= (geometry.boundingBox.max.y - geometry.boundingBox.min.y) / 2
 	center.z	= (geometry.boundingBox.max.z - geometry.boundingBox.min.z) / 2
 	geometry.vertices.forEach(function(vertex){
 		vertex.sub(center)
 	})
-	
-	// create a mesh with it
-	var material	= new THREE.MeshNormalMaterial()
+
+	var material = new THREE.MeshPhongMaterial( { color: 0xFFFF00, specular: 0x0000FF, shininess: 100} );
 	var mesh	= new THREE.Mesh(geometry, material)
-	// return mesh
 	return mesh
 }
+
+/*
+var material	= new THREE.MeshNormalMaterial()
+const color6 = new THREE.Color("0xFF0000");
+
+var material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } )
+*/
